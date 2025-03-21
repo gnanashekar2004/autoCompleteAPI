3012f78db5a1b60c9a85c23077de7a3453853305
Shekar2004 <gnanashekar2004@example.com>
2025-03-21 13:42:29 +0530

    basic exploration done

---
+++ autocomplete.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const BASE_URL = 'http://35.200.185.69:8000/v1/autocomplete';
+
+async function testAPI(query) {
+    try {
+        const response = await axios.get(`${BASE_URL}?query=${query}`);
+        console.log(`Query: ${query} -> Response:`, response.data);
+        fs.appendFileSync('api_responses.log', `Query: ${query} -> ${JSON.stringify(response.data)}\n`);
+    } catch (error) {
+        console.error(`Error querying '${query}':`, error.response ? error.response.data : error.message);
+        fs.appendFileSync('api_errors.log', `Query: ${query} -> ${error.message}\n`);
+    }
+}
+
+// Test with a few queries
+testAPI('a');
+testAPI('b');
+testAPI('c');
